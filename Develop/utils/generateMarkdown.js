289159077343a9aa8d67a97702398f4163679f29
EleazarF@imac <eleazar.fuentes@gmail.com>
2023-08-08 20:14:45 -0600

    Added function that returns the license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,7 +10,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT License":
+      return "https://img.shields.io/badge/license-MIT-green";
+    case "Apache Licence 2.0":
+      return "https://img.shields.io/badge/license-Apache-green"
+    case "GNU GPLv3":
+      return "https://img.shields.io/badge/license-GPL-green"
+    default:
+      return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
